9e90872077093a585e1111a348139f8f9bb62447
Lattefy <lattefy.ai@gmail.com>
2024-08-28 22:34:09 -0300

    software loader
    Implemented a loader:
    - start: display: block
    - end (after fetching clients): display: none

--- dashboard/assets/dashboard.js
+++ dashboard/assets/dashboard.js
@@ -4,9 +4,9 @@
 /* -------------------------------------- DATABASE CONNECTION -------------------------------------- */
 /* --------------------------------------------------------------------------------------------------*/
 
-// const apiUrl = 'http://localhost:3089'
+const apiUrl = 'http://localhost:3089'
 // const apiUrl = 'https://lattefy-server.glitch.me'
-const apiUrl = 'https://backend-5v26.onrender.com'
+// const apiUrl = 'https://backend-5v26.onrender.com'
 
 // Fetch all clients
 async function getAll() {
@@ -339,16 +339,24 @@ function downloadPDF(clients) {
 
 // DOM Content Load
 document.addEventListener('DOMContentLoaded', async function () {
+  
   const clients = await getAll()
+
+  // Loader
+  var loader = document.getElementById("loader")
+  loader.style.display = "none"
+
 
   // Dashboard
   if (document.getElementById('dashboard')) {
+
     displayClientCount(clients)
     displayDiscountsGotten(clients)
     displayDiscountsClaimed(clients)
     sentimentAnalysis(clients)
     displayAverageExpenditure(clients)
     displayTotalProfit(clients)
+
   }
 
   // Clients
