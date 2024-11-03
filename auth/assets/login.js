83d6ad505cc6d6b012268563a0052c0fc04b4147
Lattefy <lattefy.ai@gmail.com>
2024-11-03 00:39:24 -0300

    updated authUrl

--- auth/assets/login.js
+++ auth/assets/login.js
@@ -5,7 +5,7 @@
 /* --------------------------------------------------------------------------------------------------*/
 
 // const authUrl = 'http://localhost:3089'
-const authUrl = 'https://backend-v1-2-63a1.onrender.com'
+const authUrl = 'https://backend-v2-esfc.onrender.com'
 
 // assets/auth.js
 
