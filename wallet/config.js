3376281b6991c7c30c902eff091817dccc023c8a
Juani Gomez <88172215+juanigomez@users.noreply.github.com>
2025-02-21 14:03:47 -0300

    server deployment

--- wallet/config.js
+++ wallet/config.js
@@ -1,4 +1,4 @@
 // Lattefy's wallet configuration script
 
 // const apiUrl = 'http://localhost:3068'
-const apiUrl = 'https://backend-6y4u.onrender.com'
+const apiUrl = 'https://backend-4iz9.onrender.com'
