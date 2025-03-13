0b411d21463ef6584f6e97f01137869c357a5d7e
Juani Gomez <88172215+juanigomez@users.noreply.github.com>
2025-03-12 23:05:14 -0300

    cards section + nav burger

--- assets/script.js
+++ assets/script.js
@@ -1,82 +1,123 @@
 // Lattefy | Uy Software Company - landing page
 
 
-// Card Info. Animation
 
-// FAQs Animation
+
+
 
 document.addEventListener('DOMContentLoaded', function () {
-  const faqs = document.querySelectorAll('.faq')
 
-  faqs.forEach(faq => {
-      const header = faq.querySelector('.header i')
-      header.addEventListener('click', () => {
-          faqs.forEach(item => {
-              if (item !== faq) {
-                  item.classList.remove('active')
-              }
-          })
+  // NavBar Menu
+  if (document.getElementById('nav')) {
+    burger = document.querySelector(".burger")
+    navBar = document.querySelector(".menu")
+  
+    burger.onclick = function(){
+        navBar.classList.toggle("active")
+        burger.classList.toggle("active")
+    }
+  
+    navBar.onclick = function(){
+        navBar.classList.toggle("active")
+        burger.classList.toggle("active")
+    }
+  }
 
-          faq.classList.toggle('active')
-      })
-  })
+  // On Scroll effects
+  window.addEventListener('scroll', reveal)
+
+  function reveal(){
+    var reveals = document.querySelectorAll('.reveal')
+
+    for(var i = 0; i < reveals.length; i++){
+      var windowheight = window.innerHeight
+      var revealtop = reveals[i].getBoundingClientRect().top
+      var revealpoint = 150
+
+      if(revealtop < windowheight - revealpoint){
+        reveals[i].classList.add('active')
+      }else{
+        reveals[i].classList.remove('active')
+      }
+    }
+  }
+
+  // Card Selector
+  if (document.getElementById('cards')) {
+    const toggleSwitch = document.getElementById("toggle-switch");
+    const options = toggleSwitch.querySelectorAll(".option");
+    const pointsCard = document.getElementById("points-card");
+    const giftCard = document.getElementById("gift-card");
+    const discountCard = document.getElementById("discount-card"); 
+
+    function updateCardsDisplay(selectedOption) {
+      pointsCard.classList.add("hidden");
+      giftCard.classList.add("hidden");
+      discountCard.classList.add("hidden");
+
+      if (selectedOption === "cuponera") {
+          pointsCard.classList.remove("hidden");
+      } else if (selectedOption === "regalo") {
+          giftCard.classList.remove("hidden");
+      } else if (selectedOption === "discount") {
+          discountCard.classList.remove("hidden");
+      }
+    }
+
+    options.forEach((option, index) => {
+        option.addEventListener("click", function () {
+            // Remove active class from all options
+            options.forEach(opt => opt.classList.remove("active"));
+            // Add active class to the clicked option
+            option.classList.add("active");
+
+            // Update toggle background position
+            toggleSwitch.classList.remove("active-1", "active-2", "active-3");
+            toggleSwitch.classList.add(`active-${index + 1}`);
+
+            // Update card display based on selection
+            updateCardsDisplay(option.dataset.option);
+        });
+    });
+
+    // Update Display on Window Resize
+    window.addEventListener("resize", () => {
+        const activeOption = document.querySelector(".option.active");
+        updateCardsDisplay(activeOption.dataset.option);
+    });
+
+    // Initial Display Setup
+    updateCardsDisplay(document.querySelector(".option.active").dataset.option);
+  }
+
+  // FAQs Animation
+  if (document.getElementById('faqs')) {
+    const faqs = document.querySelectorAll('.faq')
+
+    faqs.forEach(faq => {
+        const header = faq.querySelector('.header i')
+        header.addEventListener('click', () => {
+            faqs.forEach(item => {
+                if (item !== faq) {
+                    item.classList.remove('active')
+                }
+            })
+
+            faq.classList.toggle('active')
+        })
+    })
+  }
 })
 
 
-// Confetti Animation:
-
-function createConfetti() {
-  const confettiContainer = document.querySelector('.news')
-  const confettiColors = ['#1DB954', '#1ed760', '#1aa34a', '#1b9e46', '#189541', '#148c3d']
-  const confettiCount = 50
-
-  for (let i = 0; i < confettiCount; i++) {
-    const confetti = document.createElement('div')
-    confetti.classList.add('confetti')
-    confetti.style.left = `${Math.random() * 100}vw`
-    confetti.style.animationDuration = `${Math.random() * 3 + 2}s`
-    confetti.style.backgroundColor = confettiColors[Math.floor(Math.random() * confettiColors.length)]
-    confettiContainer.appendChild(confetti)
-  }
-}
-createConfetti()
 
 
-// NavBar - toggle Responsive Menu:
-
-burger = document.querySelector(".burger")
-navBar = document.querySelector(".menu")
-
-burger.onclick = function(){
-    navBar.classList.toggle("active")
-    burger.classList.toggle("active")
-}
-
-navBar.onclick = function(){
-    navBar.classList.toggle("active")
-    burger.classList.toggle("active")
-}
 
 
-// On Scroll effects
 
-window.addEventListener('scroll', reveal)
 
-function reveal(){
-  var reveals = document.querySelectorAll('.reveal')
 
-  for(var i = 0; i < reveals.length; i++){
-    var windowheight = window.innerHeight
-    var revealtop = reveals[i].getBoundingClientRect().top
-    var revealpoint = 150
 
-    if(revealtop < windowheight - revealpoint){
-      reveals[i].classList.add('active')
-    }else{
-      reveals[i].classList.remove('active')
-    }
-  }
-}
 
 
 // var formBtn = document.getElementById("form-btn")
