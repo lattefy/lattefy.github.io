46fb6f66ef54d235699bb7996c91d3ddfe67b9aa
Juani Gomez <88172215+juanigomez@users.noreply.github.com>
2025-03-20 00:07:02 -0300

    Update config.js api url

--- dashboard/config.js
+++ dashboard/config.js
@@ -1,4 +1,3 @@
 // Lattefy's dashboard config file
 
-const apiUrl = 'http://localhost:3068'
-// const apiUrl = 'https://backend-6y4u.onrender.com'
+const apiUrl = 'https://backend-4iz9.onrender.com'
